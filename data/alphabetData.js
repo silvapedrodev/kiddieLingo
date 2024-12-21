0050b165a44ed4464f9af5d973a84868cf70af93
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2024-12-21 13:58:05 -0300

    add alphabet

---
+++ data/alphabetData.js
@@ -0,0 +1,159 @@
+export const alphabetData = [
+  {
+    title: "A a",
+    imgSrc: "src/public/img/alphabet/apple.svg",
+    imgAlt: "Apple",
+    paragraph: "apple"
+  },
+  {
+    title: "B b",
+    imgSrc: "src/public/img/alphabet/bird.svg",
+    imgAlt: "Bird",
+    paragraph: "bird"
+  },
+  {
+    title: "C d",
+    imgSrc: "src/public/img/alphabet/cat.svg",
+    imgAlt: "Cat",
+    paragraph: "cat"
+  },
+  {
+    title: "D d",
+    imgSrc: "src/public/img/alphabet/dog.svg",
+    imgAlt: "Dog",
+    paragraph: "dog"
+  },
+  {
+    title: "E e",
+    imgSrc: "src/public/img/alphabet/egg.svg",
+    imgAlt: "Egg",
+    paragraph: "egg"
+  },
+  {
+    title: "F f",
+    imgSrc: "src/public/img/alphabet/fox.svg",
+    imgAlt: "Fox",
+    paragraph: "fox"
+  },
+  {
+    title: "G g",
+    imgSrc: "src/public/img/alphabet/giraffe.svg",
+    imgAlt: "Giraffe",
+    paragraph: "giraffe"
+  },
+  {
+    title: "H h",
+    imgSrc: "src/public/img/alphabet/heart.svg",
+    imgAlt: "Heart",
+    paragraph: "heart"
+  },
+  {
+    title: "I i",
+    imgSrc: "src/public/img/alphabet/ice.svg",
+    imgAlt: "Ice",
+    paragraph: "ice"
+  },
+  {
+    title: "J J",
+    imgSrc: "src/public/img/alphabet/juice.svg",
+    imgAlt: "Juice",
+    paragraph: "juice"
+  },
+  {
+    title: "K k",
+    imgSrc: "src/public/img/alphabet/king.svg",
+    imgAlt: "King",
+    paragraph: "king"
+  },
+  {
+    title: "L l",
+    imgSrc: "src/public/img/alphabet/lion.svg",
+    imgAlt: "Lion",
+    paragraph: "lion"
+  },
+  {
+    title: "M m",
+    imgSrc: "src/public/img/alphabet/music.svg",
+    imgAlt: "Music",
+    paragraph: "music"
+  },
+  {
+    title: "N n",
+    imgSrc: "src/public/img/alphabet/nut.svg",
+    imgAlt: "Nut",
+    paragraph: "nut"
+  },
+  {
+    title: "O o",
+    imgSrc: "src/public/img/alphabet/owl.svg",
+    imgAlt: "Owl",
+    paragraph: "owl"
+  },
+  {
+    title: "P p",
+    imgSrc: "src/public/img/alphabet/pencil.svg",
+    imgAlt: "Pencil",
+    paragraph: "pencil"
+  },
+  {
+    title: "Q q",
+    imgSrc: "src/public/img/alphabet/queen.svg",
+    imgAlt: "Queen",
+    paragraph: "queen"
+  },
+  {
+    title: "R r",
+    imgSrc: "src/public/img/alphabet/robot.svg",
+    imgAlt: "Robot",
+    paragraph: "robot"
+  },
+  {
+    title: "S s",
+    imgSrc: "src/public/img/alphabet/snake.svg",
+    imgAlt: "Snake",
+    paragraph: "snake"
+  },
+  {
+    title: "T t",
+    imgSrc: "src/public/img/alphabet/turtle.svg",
+    imgAlt: "Turtle",
+    paragraph: "turtle"
+  },
+  {
+    title: "U u",
+    imgSrc: "src/public/img/alphabet/unicorn.svg",
+    imgAlt: "Unicorn",
+    paragraph: "unicorn"
+  },
+  {
+    title: "V v",
+    imgSrc: "src/public/img/alphabet/violin.svg",
+    imgAlt: "Violin",
+    paragraph: "violin"
+  },
+  {
+    title: "W w",
+    imgSrc: "src/public/img/alphabet/watch.svg",
+    imgAlt: "Watch",
+    paragraph: "watch"
+  },
+  {
+    title: "X x",
+    imgSrc: "src/public/img/alphabet/x-ray.svg",
+    imgAlt: "X-ray",
+    paragraph: "x-ray"
+  },
+  {
+    title: "Y y",
+    imgSrc: "src/public/img/alphabet/yogurt.svg",
+    imgAlt: "Yogurt",
+    paragraph: "yogurt"
+  },
+  {
+    title: "Z z",
+    imgSrc: "src/public/img/alphabet/zebra.svg",
+    imgAlt: "Zebra",
+    paragraph: "zebra"
+  }
+ 
+]
